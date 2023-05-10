8814c8fbf16d64f21d859483d5fa32d299c99461
Allen <76972508+vanbats1122@users.noreply.github.com>
2023-05-10 15:09:01 -0400

    post page completed

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className="single">
+      <div className="content">
+        <img src="https://cdn.pixabay.com/photo/2023/05/03/09/16/rooster-7967053_1280.jpg"/>
+        <div className="user">
+          <img src="https://cdn.pixabay.com/photo/2016/06/09/20/38/woman-1446557_1280.jpg"/>
+        </div>
+      </div>
+      <div className="menu">m</div>
+    </div>
   )
 }
 
